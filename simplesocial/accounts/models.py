119810a6ca2a5eaeddaa2a3070f58fe69a4e791a
Hani Alsabe <hanialsab3@gmail.com>
2022-05-05 15:36:18 +0300

    Models Fiedls

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -12,12 +12,19 @@ from django.utils import timezone
 class University(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     website = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, null=True)
+    phone = models.CharField(max_length=120, null=True)
+    location = models.CharField(max_length=120, null=True)
+    about = models.TextField(max_length=256, null=True)
 
     def __str__(self):
-        return "University: " + self.user.get_full_name()
+        if self.name==None:
+            return "ERROR-UNIVERSITY NAME IS NULL"
+        return self.name
 
 class Student(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     application = models.CharField(max_length=120)
+    uni = models.ManyToManyField(University, null=True)
     def __str__(self):
         return "Student: " + self.user.get_full_name()
