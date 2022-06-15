f24c3981faf3685b96315b7926ddfea921ec8716
hanialsab3 <hanialsab3@gmail.com>
2022-06-15 19:35:29 +0300

    is_authenticated to all pages that need authentication

--- simplesocial/accounts/urls.py
+++ simplesocial/accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # url(r'login/$',
     #     auth_views.LoginView.as_view(template_name='accounts/login.html'),
     #     name='login'),
-    url(r'logout/$',auth_views.LogoutView.as_view(),name='logout'),
+    # url(r'logout/$',auth_views.LogoutView.as_view(),name='logout'),
     path('signup/',SignUp.as_view(),name='signup'),
 
 ]
